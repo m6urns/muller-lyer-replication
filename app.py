56f543c3c6884a446ca29d93356e4627a08eadf0
Matt Burns <m6urns@gmail.com>
2024-11-01 19:55:20 -0600

    Try out some simple pre caching for svg images

--- app.py
+++ app.py
@@ -12,13 +12,6 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 app.config['PREFERRED_URL_SCHEME'] = 'https'
 
-# Add security headers
-# @app.after_request
-# def add_security_headers(response):
-#     response.headers['Content-Security-Policy'] = "default-src 'self' https: 'unsafe-inline' 'unsafe-eval'; img-src 'self' data: https:; style-src 'self' https: 'unsafe-inline'; script-src 'self' https: 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com;"
-#     response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
-#     response.headers['X-Content-Type-Options'] = 'nosniff'
-#     return response
 @app.after_request
 def add_security_headers(response):
     response.headers['Content-Security-Policy'] = (
@@ -68,7 +61,7 @@ def start_quiz():
 @app.route('/quiz/<int:quiz_id>')
 def quiz(quiz_id):
     if 'user_id' not in session:
-        return redirect(url_for('index', _scheme='https', _external=True))
+        return redirect(url_for('index'))
     
     quizzes = load_quiz_config()
     if quiz_id < len(quizzes):
@@ -76,20 +69,19 @@ def quiz(quiz_id):
         current_image_index = session.get('current_image_index', 0)
         
         if current_image_index < len(quiz['images']):
-            image = quiz['images'][current_image_index]
-            # Ensure image URL is HTTPS
-            image_url = url_for('static', 
-                              filename=image['url'].replace('/static/', ''),
-                              _scheme='https',
-                              _external=True)
+            # Process all image URLs to ensure they're absolute
+            for img in quiz['images']:
+                if 'url' in img:
+                    img['url'] = url_for('static', 
+                                       filename=img['url'].replace('/static/', ''),
+                                       _external=True)
             
             return render_template('quiz.html', 
                                  quiz=quiz, 
-                                 image=image,
-                                 image_url=image_url,
+                                 image=quiz['images'][current_image_index],
                                  image_index=current_image_index)
         else:
-            return redirect(url_for('thank_you', _scheme='https', _external=True))
+            return redirect(url_for('thank_you'))
     else:
         return "Quiz not found", 404
 
